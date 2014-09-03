c4d638cf385443c24e592dde1aa45e3f4f1e330f
Donald Vetal <donaldvetal@gmail.com>
2014-09-03 20:30:21 +0200

    hey

--- Grants/MLscript.py
+++ Grants/MLscript.py
@@ -14,22 +14,20 @@ import numpy as np
 import os
 import datetime as dt
 from sklearn import preprocessing
+from sklearn.ensemble import RandomForestClassifier
 
 os.chdir('PublicProjects/Grants')
 
 MLready = pd.read_csv('data/MLready.csv')
-print MLready.shape
 # 1 Split Test and train
 
 # 1.1 convert MLready.Startdate to dates
 
 MLready['Startdate'] = pd.to_datetime(pd.Series(MLready['Startdate']))
-print MLready.shape
 
 #Build Cross Validataion Function:
 #This function is special because we are attempting to keep each of the folds bounded
 #to a time range.
-
 
 def createFolds(someDF,folds,holdoutDate):
 	'''
@@ -52,7 +50,9 @@ def createFolds(someDF,folds,holdoutDate):
 	print train.shape
 	return train, test
 
-train, test = createFolds(MLready,folds=4,holdoutDate='01/01/08')
+folds = 4
+
+train, test = createFolds(MLready,folds=folds,holdoutDate='01/01/08')
 
 ###############################################################################
 
@@ -62,57 +62,41 @@ features = [col for col in MLready.columns if col not in ['GrantStatus',
 #Generate RF Fit
 clf = RandomForestClassifier(n_jobs = -1)
 RFfit = clf.fit(train[features],train['GrantStatus'])
-#print 'Accuracy: ', clf.score(test[features],test['GrantStatus'])
 
-
-#foldTrain = train[train['fold'] == 1]
-#print 'Accuracy: ', clf.score(foldTrain[features],foldTrain['GrantStatus'])
-
-performanceList = []
-for m in range(1,folds+1):
-	foldTrain = train[train['fold'] == m]
-	Response = clf.predict(foldTrain[features]) == foldTrain['GrantStatus']
-	foldTrain['Response'] = Response
-	resultArray = foldTrain['Response'].groupby(foldTrain['GrantId']).sum().apply(float)/foldTrain.groupby(foldTrain['GrantId']).size().apply(float)
-	performanceList.append(sum(resultArray)/len(resultArray))
+def randomForest(folds,train,test,features):
+	performanceList = []
+	RFfit = []
+	for m in range(1,folds+1):
+		#Generate this fold's test and train
+		foldTrain = train[train['fold'] != m]
+		foldTest = train[train['fold'] == m]
+		#fit the train for this fold
+		clf = RandomForestClassifier(n_jobs = -1)
+		RFfit.append(clf.fit(foldTrain[features],foldTrain['GrantStatus']))
+		#Test the fit against the fold's test
+		Response = clf.predict(foldTest[features]) == foldTest['GrantStatus']
+		foldTest['Response'] = Response
+		#Out of the response groupby GrantId so we get one record for each grantID, and
+		#sum the result and divide by the total number of records in a single GrantId
+		resultArray = foldTest['Response'].groupby(foldTest['GrantId']).sum().apply(float)/foldTest.groupby(foldTest['GrantId']).size().apply(float)
+		#append performance for this fold to a list
+		performanceList.append(sum(resultArray)/len(resultArray))
+		print 'In Sample Performance: for fold %d: %f' % (m, np.mean(performanceList))
+	### TEST ###
+	Response = clf.predict(test[features]) == test['GrantStatus']   
+	test['Response'] = Response
+	#Out of the response groupby GrantId so we get one record for each grantID, and
+	#sum the result and divide by the total number of records in a single GrantId
+	resultArray = test['Response'].groupby(test['GrantId']).sum().apply(float)/test.groupby(test['GrantId']).size().apply(float)
+	#append performance for this fold to a list
+	testResult = sum(resultArray)/len(resultArray)
+	print 'Out of Sample Performance: ', testResult	
+	return performanceList
 
 
 ###TEST RUN ####
-performanceList = []
-Response = clf.predict(test[features]) == test['GrantStatus']
-test['Response'] = Response
-resultArray = test['Response'].groupby(test['GrantId']).sum().apply(float)/test.groupby(test['GrantId']).size().apply(float)
-performanceList.append(sum(resultArray)/len(resultArray))
-
-
-def doRF(someDF):
-	from sklearn.ensemble import RandomForestClassifier
-
-
-def measurePerformance ():
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#performanceList = []
+#Response = clf.predict(test[features]) == test['GrantStatus']
+#test['Response'] = Response
+#resultArray = test['Response'].groupby(test['GrantId']).sum().apply(float)/test.groupby(test['GrantId']).size().apply(float)
+#performanceList.append(sum(resultArray)/len(resultArray))
