309ac4981c3f7030e5573561fb72aa848b7effba
Donald Vetal <donaldvetal@gmail.com>
2014-09-04 13:51:32 +0200

    added knn

--- Grants/MLscript.py
+++ Grants/MLscript.py
@@ -14,7 +14,7 @@ import numpy as np
 import os
 import datetime as dt
 from sklearn import preprocessing
-from sklearn.ensemble import RandomForestClassifier
+
 import matplotlib.pyplot as plt
 
 os.chdir('PublicProjects/Grants')
@@ -49,11 +49,13 @@ def createFolds(someDF,folds,holdoutDate):
 	print train.shape
 	return train, test
 
+
 def randomForest(folds,train,test,features):
 	'''
 	Performs random forest with cross validation based upon a list of features chosen
 	from the data set.
 	'''
+	from sklearn.ensemble import RandomForestClassifier
 	clf = RandomForestClassifier(n_jobs = -1)
 
 	performanceList = []
@@ -85,6 +87,44 @@ def randomForest(folds,train,test,features):
 	testResult = sum(resultArray)/len(resultArray)	
 	return Bestfit, np.mean(performanceList), testResult
 
+
+def knnRun(folds,train,test,features):
+	'''
+	Performs random forest with cross validation based upon a list of features chosen
+	from the data set.
+	'''
+	from sklearn.neighbors import KNeighborsClassifier
+	clf = KNeighborsClassifier(n_neighbors = 5)
+	performanceList = []
+	RFfit = []
+	for m in range(1,folds+1):
+		#Generate this fold's test and train
+		foldTrain = train[train['fold'] != m]
+		foldTest = train[train['fold'] == m]
+		#fit the train for this fold	
+		RFfit.append(clf.fit(foldTrain[features],foldTrain['GrantStatus']))
+		#Test the fit against the fold's test
+		Response = clf.predict(foldTest[features]) == foldTest['GrantStatus']
+		foldTest['Response'] = Response
+		#Out of the response groupby GrantId so we get one record for each grantID, and
+		#sum the result and divide by the total number of records in a single GrantId
+		resultArray = foldTest['Response'].groupby(foldTest['GrantId']).sum().apply(float)/foldTest.groupby(foldTest['GrantId']).size().apply(float)
+		#append performance for this fold to a list
+		performanceList.append(sum(resultArray)/len(resultArray))
+	#	print 'In Sample Performance: for fold %d: %f' % (m, np.mean(performanceList))
+	Bestfitfold = performanceList.index(max(performanceList)) + 1
+	BestfitTrain = train[train['fold'] != Bestfitfold]
+	Bestfit = clf.fit(BestfitTrain[features],BestfitTrain['GrantStatus'])
+	testResponse = clf.predict(test[features]) == test['GrantStatus']
+	test['Response'] = testResponse
+	#Out of the response groupby GrantId so we get one record for each grantID, and
+	#sum the result and divide by the total number of records in a single GrantId
+	resultArray = test['Response'].groupby(test['GrantId']).sum().apply(float)/test.groupby(test['GrantId']).size().apply(float)
+	#append performance for this fold to a list
+	testResult = sum(resultArray)/len(resultArray)	
+	return Bestfit, np.mean(performanceList), testResult
+
+
 def bestFeatureList(top,fit,frame):
 	'''
 	Based upon a fit this function determines the column names for the most significant features.  This list can be used as
@@ -112,7 +152,7 @@ trainResultList = []
 testResultList = []
 
 #Get an initial fit with many features
-someFit, trainResult, testResult = randomForest(folds,train,test,features)
+someFit, trainResult, testResult = knnRun(folds,train,test,features)
 
 numFeatures.append(len(features))
 trainResultList.append(trainResult)
