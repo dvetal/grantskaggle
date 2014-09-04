7e37f94fc8cdaebe7df3402a9195fe012b042cd0
Donald Vetal <donaldvetal@gmail.com>
2014-09-04 12:42:41 +0200

    script runs feature grid search

--- Grants/MLscript.py
+++ Grants/MLscript.py
@@ -15,19 +15,18 @@ import os
 import datetime as dt
 from sklearn import preprocessing
 from sklearn.ensemble import RandomForestClassifier
+import matplotlib.pyplot as plt
 
 os.chdir('PublicProjects/Grants')
 
+#LOAD DATA
 MLready = pd.read_csv('data/MLready.csv')
-# 1 Split Test and train
-
-# 1.1 convert MLready.Startdate to dates
-
+#Convert MLready.Startdate to dates
 MLready['Startdate'] = pd.to_datetime(pd.Series(MLready['Startdate']))
 
-#Build Cross Validataion Function:
-#This function is special because we are attempting to keep each of the folds bounded
-#to a time range.
+###############
+## FUNCTIONS ##
+###############
 
 def createFolds(someDF,folds,holdoutDate):
 	'''
@@ -50,28 +49,20 @@ def createFolds(someDF,folds,holdoutDate):
 	print train.shape
 	return train, test
 
-folds = 4
-
-train, test = createFolds(MLready,folds=folds,holdoutDate='01/01/08')
-
-###############################################################################
-
-features = [col for col in MLready.columns if col not in ['GrantStatus',
-'GrantId','PersonID','folds','Startdate','Role','WithPHD']]
-
-#Generate RF Fit
-clf = RandomForestClassifier(n_jobs = -1)
-RFfit = clf.fit(train[features],train['GrantStatus'])
-
 def randomForest(folds,train,test,features):
+	'''
+	Performs random forest with cross validation based upon a list of features chosen
+	from the data set.
+	'''
+	clf = RandomForestClassifier(n_jobs = -1)
+
 	performanceList = []
 	RFfit = []
 	for m in range(1,folds+1):
 		#Generate this fold's test and train
 		foldTrain = train[train['fold'] != m]
 		foldTest = train[train['fold'] == m]
-		#fit the train for this fold
-		clf = RandomForestClassifier(n_jobs = -1)
+		#fit the train for this fold	
 		RFfit.append(clf.fit(foldTrain[features],foldTrain['GrantStatus']))
 		#Test the fit against the fold's test
 		Response = clf.predict(foldTest[features]) == foldTest['GrantStatus']
@@ -81,22 +72,91 @@ def randomForest(folds,train,test,features):
 		resultArray = foldTest['Response'].groupby(foldTest['GrantId']).sum().apply(float)/foldTest.groupby(foldTest['GrantId']).size().apply(float)
 		#append performance for this fold to a list
 		performanceList.append(sum(resultArray)/len(resultArray))
-		print 'In Sample Performance: for fold %d: %f' % (m, np.mean(performanceList))
-	### TEST ###
-	Response = clf.predict(test[features]) == test['GrantStatus']   
-	test['Response'] = Response
+	#	print 'In Sample Performance: for fold %d: %f' % (m, np.mean(performanceList))
+	Bestfitfold = performanceList.index(max(performanceList)) + 1
+	BestfitTrain = train[train['fold'] != Bestfitfold]
+	Bestfit = clf.fit(BestfitTrain[features],BestfitTrain['GrantStatus'])
+	testResponse = clf.predict(test[features]) == test['GrantStatus']
+	test['Response'] = testResponse
 	#Out of the response groupby GrantId so we get one record for each grantID, and
 	#sum the result and divide by the total number of records in a single GrantId
 	resultArray = test['Response'].groupby(test['GrantId']).sum().apply(float)/test.groupby(test['GrantId']).size().apply(float)
 	#append performance for this fold to a list
-	testResult = sum(resultArray)/len(resultArray)
-	print 'Out of Sample Performance: ', testResult	
-	return performanceList
+	testResult = sum(resultArray)/len(resultArray)	
+	return Bestfit, np.mean(performanceList), testResult
+
+def bestFeatureList(top,fit,frame):
+	'''
+	Based upon a fit this function determines the column names for the most significant features.  This list can be used as
+	an imput into another function to specify the features you want to fit and predict on.
+	'''
+	importanceValues = fit.feature_importances_
+	indices = np.argsort(importanceValues)[::-1]
+	return list(frame.columns[indices][:top])
+
+##############
+## WORKFLOW ##
+##############
+
+#Assign number of folds for crossvalidation
+folds = 4
+
+train, test = createFolds(MLready,folds=folds,holdoutDate='01/01/08')
+
+#Assign features for machine learning to a list of column names
+features = [col for col in MLready.columns if col not in ['GrantStatus',
+'GrantId','PersonID','folds','Startdate','Role','WithPHD']]
+
+numFeatures = []
+trainResultList = []
+testResultList = []
+
+#Get an initial fit with many features
+someFit, trainResult, testResult = randomForest(folds,train,test,features)
+
+numFeatures.append(len(features))
+trainResultList.append(trainResult)
+testResultList.append(testResult)
+
+featureSpaceSize = [1,5,10,15,20,50,100,150,200,300,350]
+for n in featureSpaceSize:
+	NewFeatures = bestFeatureList(top = n, fit = someFit, frame = train[features])
+	someNewFit, trainResult, testResult = randomForest(folds = folds, train = train, test = test, features = NewFeatures)
+	numFeatures.append(n)
+	trainResultList.append(trainResult)
+	testResultList.append(testResult)
+
+#mess with hyperparameters
+
+featurereductionTrain = plt.plot(numFeatures,trainResultList)
+plt.show()
+
+featurereductionTest = plt.plot(numFeatures,testResultList)
+plt.show()
 
 
-###TEST RUN ####
-#performanceList = []
-#Response = clf.predict(test[features]) == test['GrantStatus']
-#test['Response'] = Response
-#resultArray = test['Response'].groupby(test['GrantId']).sum().apply(float)/test.groupby(test['GrantId']).size().apply(float)
-#performanceList.append(sum(resultArray)/len(resultArray))
+### THINGS TO DO ####
+
+#Normalize data
+#grid search over RF hyper parameters
+#try other ML methods
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
